cda2313025784f9bae3c74e9c207d1cec5bc659a
kovacsand <47793048+kovacsand@users.noreply.github.com>
2022-05-05 12:52:39 +0200

    Lots of packages and interfaces, few empty classes

---
+++ src/shared/utils/Subject.java
@@ -0,0 +1,10 @@
+package shared.utils;
+
+import java.beans.PropertyChangeListener;
+
+public interface Subject
+{
+  void addListener(String propertyName, PropertyChangeListener listener);
+
+  void removeListener(String propertyName, PropertyChangeListener listener);
+}
