f9b32fa4d1213b3b4b64be80f9f2054249b7e6e1
JcullenNZ <jcullen.dk@gmail.com>
2022-05-09 12:24:51 +0200

    Added  - test outlines for LoginViewController  - test outlines for LoginViewModel

--- test/client/view/login/LoginViewControllerTest.java
+++ test/client/view/login/LoginViewControllerTest.java
@@ -12,100 +12,78 @@ import static org.junit.jupiter.api.Assertions.*;
 class LoginViewControllerTest
 {
 
-  private LoginViewModel loginVM;
-  private LoginViewController loginVC;
+  private String usernameField;
+  private String passwordField;
 
-  @BeforeEach public void setUp()
-  {
-  }
 
   @Test public void loginSuccess()
   {
     //arrange
-    StringProperty username = new SimpleStringProperty();
-    StringProperty password = new SimpleStringProperty();
-    loginVM.usernameProperty.bindBidirectional(username);
-    loginVM.passwordProperty.bindBidirectional(password);
-
+      //usernameField = correct
+      //passwordField = correct
     //act
-    username.setValue("James");
-    password.setValue("123");
-    vm.login();
-
+      //loginVM.login()
 
     //assert
+      //
+    System.out.println(usernameField + " " + passwordField);
     System.out.println("ViewChange");
   }
 
-  @Test public void usernamePasswordNoMatch()
+ @Test public void usernamePasswordNoMatch()
   {
     //arrange
-    StringProperty username = new SimpleStringProperty();
-    StringProperty password = new SimpleStringProperty();
-    loginVM.usernameProperty.bindBidirectional(username);
-    loginVM.passwordProperty.bindBidirectional(password);
+      //userField = correct(NotNull)
+      //passwordField = correct(NotNull)
 
     //act
-    username.setValue("James");
-    password.setValue("124");
-    vm.login();
+      //loginVM.login()
 
     //Assert
+      //assertEquals(errorMessage.getValue().equals("Incorrect username and/or password, or fields can not be empty")
     System.out.println("NoViewChange");
-    System.out.println("Incorrect username and/or password, or fields can not be empty");
   }
 
   @Test public void usernameEmptyPasswordFilled()
   {
     //arrange
-    StringProperty username = new SimpleStringProperty();
-    StringProperty password = new SimpleStringProperty();
-    loginVM.usernameProperty.bindBidirectional(username);
-    loginVM.passwordProperty.bindBidirectional(password);
+      //userField = empty/null
+     //passwordField = correct
 
     //act
-    username.setValue("");
-    password.setValue("124");
-    vm.login();
+      //loginVM.login()
 
     //assert
     System.out.println("NoViewChange");
-    System.out.println("Incorrect username and/or password, or fields can not be empty");
+      //assertEquals(errorMessage.getValue().equals("Incorrect username and/or password, or fields can not be empty")
   }
 
   @Test public void usernameFilledPasswordEmpty()
   {
     //arrange
-    StringProperty username = new SimpleStringProperty();
-    StringProperty password = new SimpleStringProperty();
-    loginVM.usernameProperty.bindBidirectional(username);
-    loginVM.passwordProperty.bindBidirectional(password);
+      //userField = correct(NotNull)
+      //passwordField = empty/null
+
 
     //act
-    username.setValue("James");
-    password.setValue("");
-    vm.login();
+
 
     //assert
     System.out.println("NoViewChange");
-    System.out.println("Incorrect username and/or password, or fields can not be empty");
+     //assertEquals(errorMessage.getValue().equals("Incorrect username and/or password, or fields can not be empty"));
   }
 
   @Test public void usernameEmptyPasswordEmpty()
   {
     //arrange
-    StringProperty username = new SimpleStringProperty();
-    StringProperty password = new SimpleStringProperty();
-    loginVM.usernameProperty.bindBidirectional(username);
-    loginVM.passwordProperty.bindBidirectional(password);
+
 
     //act
-    username.setValue("");
-    password.setValue("");
-    vm.login();
+      //userField = empty/null
+      //passwordField = empty/null
 
     //assert
+      //assertEquals(errorMessage.getValue().equals("Incorrect username and/or password, or fields can not be empty"));
     System.out.println("NoViewChange");
-    System.out.println("Incorrect username and/or password, or fields can not be empty");
   }
 }
