2a25e6c1182a9121ae2cd27050fca50fdea97668
kovacsand <47793048+kovacsand@users.noreply.github.com>
2022-05-24 13:02:36 +0200

    SaleServer interface, the model interface fixed

--- src/server/networking/SaleServer.java
+++ src/server/networking/SaleServer.java
@@ -5,8 +5,6 @@ import shared.transferobjects.Receipt;
 import shared.transferobjects.Sale;
 import shared.transferobjects.Salesperson;
 
-import java.rmi.RemoteException;
-
 public interface SaleServer
 {
   Receipt finaliseSale(Sale sale, Salesperson salesperson);
